cafe0e5b5d2e835b44f03a8f9c557e9c70c34176
freedomhust <u201614891@hust.edu.cn>
2018-08-11 16:55:21 +0800

    StringBad

---
+++ 12/12.1/12.1-stringbad.h
@@ -0,0 +1,16 @@
+#include <iostream>
+#ifndef STRINGBAD_H_
+#define STRINGBAD_H_
+/* code */
+class StringBad{
+    private:
+        char *str;
+        int len;
+        static int num_strings;
+    public:
+        StringBad(const char *s);
+        StringBad();
+        ~StringBad();
+        friend std::ostream & operator<<(std::ostream &os, const StringBad * st);
+};
+#endif //STRINGBAD_H_
